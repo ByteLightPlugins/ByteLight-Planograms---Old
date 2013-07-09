82ddb0e69d64f5ba2d20122de53521cace252b79
ByteLight Plugin Repositories <plugins@bytelight.com>
2013-07-09 13:47:12 -0700

    Released on: 2013-07-09 20:47:11 +0000

--- show/page.js
+++ show/page.js
@@ -1,7 +1,2 @@
-var imageUrl = BL.getSettings().image_url_2x[0]
-
-if (BL.isPhone()) {
-  imageUrl = BL.getSettings().image_url[0]
-}
-
-$('.big-image').css('background', 'url(' + imageUrl + ') no-repeat 0')
+var imageUrl = BL.getSettings().image_url[0];
+$('.big-image').css('background', 'url(' + imageUrl + ') no-repeat 0');
