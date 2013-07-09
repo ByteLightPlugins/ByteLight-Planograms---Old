2a3875c3f8266d67803ce1ceb9288d7d8f2710dd
ByteLight Plugin Repositories <plugins@bytelight.com>
2013-07-09 11:08:17 -0700

    Released on: 2013-07-09 18:08:15 +0000

--- preview/page.js
+++ preview/page.js
@@ -1,4 +1,3 @@
-$('p').text('Hello World: this is the preview page that gets used to preview your app');
-  $('button').on('click', function() {
-  BL.displayApp(getAppId())
+$('.planogram-preview-main').on('click', function() {
+  BL.displayApp(APP.appId)
 });
--- show/page.js
+++ show/page.js
@@ -1,1 +1,7 @@
-$('p').text('Hello World: this is the main detail page for displaying your app.');
+var imageUrl = BL.getSettings().image_url_2x[0]
+
+if (BL.isPhone()) {
+  imageUrl = BL.getSettings().image_url[0]
+}
+
+$('.big-image').css('background', 'url(' + imageUrl + ') no-repeat 0')
